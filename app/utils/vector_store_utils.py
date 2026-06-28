1347da0b5d307fd4b575971b8ab454658e09f294
nyda1908 <nidhi.mpulsar@gmail.com>
2026-06-28 16:12:26 +0530

    Refactor embed_text method to accept documents

--- app/utils/vector_store_utils.py
+++ app/utils/vector_store_utils.py
@@ -1,19 +1,18 @@
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain.schema import Document
 
 
 class MyVectorStore:
     def __init__(self):
         self.embeddings = OpenAIEmbeddings()
-        pass
 
-    def embed_text(self, text_chunks):
-        Chroma.from_texts(
-            text_chunks, self.embeddings, persist_directory="../data/chroma_db"
+    def embed_text(self, documents):
+        Chroma.from_documents(
+            documents, self.embeddings, persist_directory="../data/chroma_db"
         )
 
     def get_retriever(self):
-        # load from disk
         vector_store = Chroma(
             persist_directory="../data/chroma_db", embedding_function=self.embeddings
         )
