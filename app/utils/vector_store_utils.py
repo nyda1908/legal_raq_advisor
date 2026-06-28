a707f0b0e2f8a7652d00ff003805727a17021b70
nyda1908 <nidhi.mpulsar@gmail.com>
2026-06-28 17:00:26 +0530

    Update vector_store_utils.py

--- app/utils/vector_store_utils.py
+++ app/utils/vector_store_utils.py
@@ -3,6 +3,9 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.schema import Document
 from langchain.retrievers import EnsembleRetriever
 from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
+from langchain.retrievers.document_compressors import CrossEncoderReranker
 
 
 class MyVectorStore:
@@ -27,9 +30,20 @@ class MyVectorStore:
         bm25_retriever = BM25Retriever.from_documents(self.documents)
         bm25_retriever.k = 20
 
-        # combine
+        # hybrid retriever
         ensemble_retriever = EnsembleRetriever(
             retrievers=[bm25_retriever, dense_retriever],
             weights=[0.5, 0.5]
         )
-        return ensemble_retriever
+
+        # reranker
+        cross_encoder = HuggingFaceCrossEncoder(
+            model_name="cross-encoder/ms-marco-MiniLM-L-6-v2"
+        )
+        compressor = CrossEncoderReranker(model=cross_encoder, top_n=7)
+        reranking_retriever = ContextualCompressionRetriever(
+            base_compressor=compressor,
+            base_retriever=ensemble_retriever
+        )
+
+        return reranking_retriever
