9d23e4af0f87eb8cc7202da88848afb9d2cb92b3
nyda1908 <nidhi.mpulsar@gmail.com>
2026-06-28 16:11:11 +0530

    Refactor MyTextSplitter to handle multiple documents

--- app/utils/text_splitter_utils.py
+++ app/utils/text_splitter_utils.py
@@ -1,13 +1,29 @@
+import tiktoken
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
 
 
 class MyTextSplitter:
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, documents):
+        self.documents = documents
 
     def get_text_chunks(self):
+        encoder = tiktoken.encoding_for_model("text-embedding-ada-002")
+        
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1024, chunk_overlap=200, length_function=len
+            chunk_size=512,
+            chunk_overlap=50,
+            length_function=lambda text: len(encoder.encode(text)),
         )
-        chunks = text_splitter.split_text(self.text)
+        
+        chunks = []
+        for doc in self.documents:
+            split_texts = text_splitter.split_text(doc.page_content)
+            for chunk in split_texts:
+                chunks.append(
+                    Document(
+                        page_content=chunk,
+                        metadata=doc.metadata
+                    )
+                )
         return chunks
